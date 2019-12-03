54f03a6bbb7fa56e4aaa6c9783f29cadc7d65f61
savy91 <savy91@gmail.com>
2019-12-04 00:53:10 +0100

    WIP iOS

--- ios/Classes/SwiftFlutterPaldeskPlugin.swift
+++ ios/Classes/SwiftFlutterPaldeskPlugin.swift
@@ -1,14 +1,59 @@
 import Flutter
 import UIKit
+import Paldesk
 
 public class SwiftFlutterPaldeskPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_paldesk", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterPaldeskPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    private static let METHOD_INIT = "init"
+    private static let METHOD_START_CONVERSATION = "startConversation"
+    private static let METHOD_CLEAR = "clear"
+    private static let METHOD_CREATE_ANONYMOUS_CLIENT = "createAnonymousClient"
+    private static let METHOD_CREATE_CLIENT = "createClient"
+    private let registrar: FlutterPluginRegistrar
+    
+    init(registrar: FlutterPluginRegistrar) {
+        self.registrar = registrar
+    }
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "flutter_paldesk", binaryMessenger: registrar.messenger())
+        let instance = SwiftFlutterPaldeskPlugin(registrar: registrar)
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    private var vc: UIViewController {
+        get {
+            return UIApplication.shared.keyWindow!.rootViewController!
+        }
+    }
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch (call.method) {
+        case SwiftFlutterPaldeskPlugin.METHOD_INIT:
+            let arguments = call.arguments as! [String: Any]
+            let apiKey = arguments["apiKey"] as! String
+            PaldeskSDK.initialize(apiKey: apiKey);
+            result(true)
+            break;
+        case SwiftFlutterPaldeskPlugin.METHOD_START_CONVERSATION:
+            result(true)
+            break;
+        case SwiftFlutterPaldeskPlugin.METHOD_CLEAR:
+            result(true)
+            break;
+        case SwiftFlutterPaldeskPlugin.METHOD_CREATE_ANONYMOUS_CLIENT:
+            result(true)
+            break;
+        case SwiftFlutterPaldeskPlugin.METHOD_CREATE_CLIENT:
+            let arguments = call.arguments as! [String: Any]
+            let email = arguments["email"] as! String
+            let externalId = arguments["externalId"] as! String
+            let firstName = arguments["firstName"] as! String
+            let lastName = arguments["lastName"] as! String
+            result(true)
+            break;
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+            break;
+        default:
+            result(false)
+        }
+    }
 }
--- ios/flutter_paldesk.podspec
+++ ios/flutter_paldesk.podspec
@@ -11,13 +11,13 @@ A new Flutter plugin to use Paldesk.
                        DESC
   s.homepage         = 'http://example.com'
   s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
+  s.author           = { 'Sungazer OÜ' => 'saverio@sungazer.io' }
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.public_header_files = 'Classes/**/*.h'
   s.dependency 'Flutter'
-  s.platform = :ios, '8.0'
-
+  s.dependency 'Paldesk', '0.0.1'
+  s.platform = :ios, '10.0'
   # Flutter.framework does not contain a i386 slice. Only x86_64 simulators are supported.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'VALID_ARCHS[sdk=iphonesimulator*]' => 'x86_64' }
 end
